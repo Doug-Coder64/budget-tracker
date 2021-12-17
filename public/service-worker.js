ca40afc20e2474129d28556088704cae75d1f639
Doug Peterson <douglaspeterson6@gmail.com>
2021-12-17 12:58:11 -0600

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,19 +4,20 @@ const DATA_CACHE_NAME = 'data-cache';
 const FILES_TO_CACHE = [
 	'/',
 	'/index.html',
-	'/favicon.ico',
 	'/manifest.json',
-	'/app.js',
-	'/css/style.css',
+	'/css/styles.css',
+	'/js/index.js',
+	'/js/idb.js',
 	'/icons/icon-72x72.png',
 	'/icons/icon-96x96.png',
 	'/icons/icon-128x128.png',
 	'/icons/icon-144x144.png',
+	'/icons/icon-192x192.png',
 ];
 
 // Install the service worker
-self.addEventListener('install', function (evt) {
-	evt.waitUntil(
+self.addEventListener('install', function (event) {
+	event.waitUntil(
 		caches.open(CACHE_NAME).then((cache) => {
 			console.log('Your files were pre-cached successfully!');
 			return cache.addAll(FILES_TO_CACHE);
@@ -27,8 +28,8 @@ self.addEventListener('install', function (evt) {
 });
 
 // Activate the service worker and remove old data from the cache
-self.addEventListener('activate', function (evt) {
-	evt.waitUntil(
+self.addEventListener('activate', function (event) {
+	event.waitUntil(
 		caches.keys().then((keyList) => {
 			return Promise.all(
 				keyList.map((key) => {
@@ -45,24 +46,24 @@ self.addEventListener('activate', function (evt) {
 });
 
 // Intercept fetch requests
-self.addEventListener('fetch', function (evt) {
-	if (evt.request.url.includes('/api/')) {
-		evt.respondWith(
+self.addEventListener('fetch', function (event) {
+	if (event.request.url.includes('/api/')) {
+		event.respondWith(
 			caches
 				.open(DATA_CACHE_NAME)
 				.then((cache) => {
-					return fetch(evt.request)
+					return fetch(event.request)
 						.then((response) => {
 							// If the response was good, clone it and store it in the cache.
 							if (response.status === 200) {
-								cache.put(evt.request.url, response.clone());
+								cache.put(event.request.url, response.clone());
 							}
 
 							return response;
 						})
 						.catch((err) => {
 							// Network request failed, try to get it from the cache.
-							return cache.match(evt.request);
+							return cache.match(event.request);
 						});
 				})
 				.catch((err) => console.log(err))
@@ -71,12 +72,12 @@ self.addEventListener('fetch', function (evt) {
 		return;
 	}
 
-	evt.respondWith(
-		fetch(evt.request).catch(function () {
-			return caches.match(evt.request).then(function (response) {
+	event.respondWith(
+		fetch(event.request).catch(function () {
+			return caches.match(event.request).then(function (response) {
 				if (response) {
 					return response;
-				} else if (evt.request.headers.get('accept').includes('text/html')) {
+				} else if (event.request.headers.get('accept').includes('text/html')) {
 					// return the cached home page for all requests for html pages
 					return caches.match('/');
 				}
